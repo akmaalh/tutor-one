9bd15df4b41a57bb305ef7beff7b279cd4d4f55d
Muhammad Akmal Hakim <akmalhakim2106@gmail.com>
2022-09-15 11:41:51 +0700

    menyelesaikan lab 2

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,8 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.core import serializers
+from django.http import HttpResponse
 # Create your views here.
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
@@ -8,3 +11,15 @@ def show_wishlist(request):
     'nama': 'Muhammad Akmal Hakim'
     }
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
